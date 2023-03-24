d7f265a1c495c71f9a24f9b76339fab76c312953
P-E Gros <pgros@salesforce.com>
2023-03-24 12:11:09 +0100

    new DSFR static resource loader (optional)

---
+++ force-app/main/extensions/lwc/dsfrLoaderUtl/dsfrLoaderUtl.js
@@ -0,0 +1,26 @@
+import { LightningElement, api } from 'lwc';
+
+import { loadStyle } from 'lightning/platformResourceLoader';
+import DSFR_RSC from '@salesforce/resourceUrl/dsfr';
+
+export default class DsfrLoaderUtl extends LightningElement {
+
+    //-----------------------------------------------------    
+    // Initialisation
+    //-----------------------------------------------------
+    @api isDebug = false;
+
+    //-----------------------------------------------------
+    // Initialisation
+    //-----------------------------------------------------
+    connectedCallback() {
+        if (this.isDebug) console.log('connected: START loading DSFR');
+        Promise.all([
+            loadStyle(this, DSFR_RSC + '/dsfr.min.css'),
+            loadStyle(this, DSFR_RSC + '/utility/utility.min.css')
+        ]).then(() => {
+            if (this.isDebug) console.log('connected: END DSFR loaded');
+        });
+        if (this.isDebug) console.log('connected: loading DSFR');
+    }
+}
