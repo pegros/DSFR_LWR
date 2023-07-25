4243650f4b25658a43efc8a0075dd18986a72595
P-E Gros <pgros@salesforce.com>
2023-07-25 13:09:40 +0200

    Loaded SLDS Overrides in addition to  DSFR

--- force-app/main/extensions/lwc/dsfrLoaderUtl/dsfrLoaderUtl.js
+++ force-app/main/extensions/lwc/dsfrLoaderUtl/dsfrLoaderUtl.js
@@ -2,6 +2,7 @@ import { LightningElement, api } from 'lwc';
 
 import { loadStyle } from 'lightning/platformResourceLoader';
 import DSFR_RSC from '@salesforce/resourceUrl/dsfr';
+import DSFR_SLDS_RSC from '@salesforce/resourceUrl/dsfrSlds';
 
 export default class DsfrLoaderUtl extends LightningElement {
 
@@ -14,13 +15,17 @@ export default class DsfrLoaderUtl extends LightningElement {
     // Initialisation
     //-----------------------------------------------------
     connectedCallback() {
-        if (this.isDebug) console.log('connected: START loading DSFR');
+        if (this.isDebug) console.log('connected: START loading DSFR and Overrides');
         Promise.all([
             loadStyle(this, DSFR_RSC + '/dsfr.min.css'),
-            loadStyle(this, DSFR_RSC + '/utility/utility.min.css')
+            loadStyle(this, DSFR_RSC + '/utility/utility.min.css'),
+            loadStyle(this, DSFR_SLDS_RSC)
+            //loadStyle(this, DSFR_SLDS_RSC + '/dsfrSlds.css')
         ]).then(() => {
-            if (this.isDebug) console.log('connected: END DSFR loaded');
+            if (this.isDebug) console.log('connected: END DSFR and Overrides loaded');
+        }).catch((error) => {
+            console.warn('connected: END DSFR and Overrides loading failed ',JSON.stringify(error));
         });
-        if (this.isDebug) console.log('connected: loading DSFR');
+        if (this.isDebug) console.log('connected: loading DSFR and Overrides ');
     }
 }
