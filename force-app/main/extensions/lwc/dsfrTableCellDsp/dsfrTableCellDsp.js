64f8b65feab779fc8030516def852749b5577c5f
P-E Gros <pgros@salesforce.com>
2024-11-15 16:03:05 +0100

    Added debug mode control

--- force-app/main/extensions/lwc/dsfrTableCellDsp/dsfrTableCellDsp.js
+++ force-app/main/extensions/lwc/dsfrTableCellDsp/dsfrTableCellDsp.js
@@ -7,6 +7,7 @@ export default class DsfrTableCellDsp extends LightningElement {
     //--------------------------------------------------------
     @api field;
     @api value;
+    @api isDebug;
 
     //--------------------------------------------------------
     // Technical properties
@@ -55,29 +56,29 @@ export default class DsfrTableCellDsp extends LightningElement {
     // Custom getters
     //--------------------------------------------------------
     connectedCallback() {
-        console.log('connected: START on CELL of type ',this.field?.type)
+        if (this.isDebug) console.log('connected: START on CELL of type ',this.field?.type)
         this.isButton = (this.field?.type === 'button') || (this.field.type === 'button-icon')
                         || ((this.field.type === 'action') && (this.field.typeAttributes?.rowActions?.length == 1));
-        console.log('connected: isButton init ',this.isButton);
+        if (this.isDebug)console.log('connected: isButton init ',this.isButton);
         if (this.isButton) {
             this.buttonDesc = (this.field.type == 'action' ? this.field.typeAttributes.rowActions[0] : this.field.typeAttributes);
             console.log('connected: buttonDesc init',JSON.stringify(this.buttonDesc));
         }
-        console.log('connected: END on CELL');
+        if (this.isDebug) console.log('connected: END on CELL');
     }
 
     //--------------------------------------------------------
     // Event Handlers
     //--------------------------------------------------------
     handleAction(event) {
-        console.log('handleAction: START on CELL with event',event);
+        if (this.isDebug) console.log('handleAction: START on CELL with event',event);
         event.stopPropagation();
         event.preventDefault();
         if (this.refs.actionMenu) {
-            console.log('handleAction: analysing menu state ',this.refs.menuButton);
+            if (this.isDebug) console.log('handleAction: analysing menu state ',this.refs.menuButton);
 
             if (this.refs.menuButton.getAttribute('aria-expanded')) {
-                console.log('handleAction: closing expanded menu');
+                if (this.isDebug) console.log('handleAction: closing expanded menu');
                 this.toggleMenu(event);
             }
             else {
@@ -85,10 +86,10 @@ export default class DsfrTableCellDsp extends LightningElement {
             }
         }
         else {
-            console.log('handleAction: no menu ');
+            if (this.isDebug) console.log('handleAction: no menu ');
         }
-        console.log('handleAction: action triggered (value) ',JSON.stringify(event.target.value));
-        console.log('handleAction: action triggered (data-action) ',JSON.stringify(event.target.dataset.action));
+        if (this.isDebug) console.log('handleAction: action triggered (value) ',JSON.stringify(event.target.value));
+        if (this.isDebug) console.log('handleAction: action triggered (data-action) ',JSON.stringify(event.target.dataset.action));
 
         let targetAction = event.target.value || event.target.dataset.action;
         if (this.isDebug) console.log('handleAction: typeof targetAction ', typeof targetAction);
@@ -98,13 +99,13 @@ export default class DsfrTableCellDsp extends LightningElement {
         }
         if (this.isDebug) console.log('handleAction: targetAction identified ',JSON.stringify(targetAction));
 
-        console.log('handleAction: on field',JSON.stringify(this.field));
-        console.log('handleAction: on value',JSON.stringify(this.value));
+        if (this.isDebug) console.log('handleAction: on field',JSON.stringify(this.field));
+        if (this.isDebug) console.log('handleAction: on value',JSON.stringify(this.value));
         if (targetAction) {
             const actionEvent = new CustomEvent('action', { detail: { action : targetAction } });
-            console.log('handleAction: actionEvent prepared ',JSON.stringify(actionEvent));
+            if (this.isDebug) console.log('handleAction: actionEvent prepared ',JSON.stringify(actionEvent));
             this.dispatchEvent(actionEvent);
-            console.log('handleAction: END on CELL with actionEvent',actionEvent);
+            if (this.isDebug) console.log('handleAction: END on CELL with actionEvent',actionEvent);
         }
         else {
             console.warn('handleAction: END KO on CELL / no value found');
@@ -112,30 +113,30 @@ export default class DsfrTableCellDsp extends LightningElement {
     }
 
     toggleMenu(event) {
-        console.log('toggleMenu: START for table cell',event);
+        if (this.isDebug) console.log('toggleMenu: START for table cell',event);
         event.stopPropagation();
         event.preventDefault();
 
         let actionMenu = this.refs.actionMenu;
-        console.log('toggleMenu: actionMenu found ',actionMenu);
+        if (this.isDebug) console.log('toggleMenu: actionMenu found ',actionMenu);
         let menuButton = this.refs.menuButton;
-        console.log('toggleMenu: menuButton found ',menuButton);
+        if (this.isDebug) console.log('toggleMenu: menuButton found ',menuButton);
 
         if (actionMenu) {
-            console.log('toggleMenu: current Menu classList ',actionMenu.classList);
+            if (this.isDebug) console.log('toggleMenu: current Menu classList ',actionMenu.classList);
             if (actionMenu.classList.contains('fr-collapse--expanded')) {
-                console.log('toggleMenu: closing menu');
+                if (this.isDebug) console.log('toggleMenu: closing menu');
                 actionMenu.classList.remove('fr-collapse--expanded');
                 menuButton.setAttribute('aria-expanded',false);
             }
             else {
-                console.log('toggleMenu: opening menu');
+                if (this.isDebug) console.log('toggleMenu: opening menu');
                 actionMenu.classList.add('fr-collapse--expanded');
                 menuButton.setAttribute('aria-expanded',true);
             }
-            console.log('toggleMenu: Menu classList updated ',actionMenu.classList);
+            if (this.isDebug) console.log('toggleMenu: Menu classList updated ',actionMenu.classList);
         }
 
-        console.log('toggleMenu: END for table cell');
+        if (this.isDebug) console.log('toggleMenu: END for table cell');
     }
 }
--- force-app/main/extensions/lwc/dsfrTableRowDsp/dsfrTableRowDsp.js
+++ force-app/main/extensions/lwc/dsfrTableRowDsp/dsfrTableRowDsp.js
@@ -4,21 +4,24 @@ export default class DsfrTableRowDsp extends LightningElement {
 
     @api fields;
     @api value;
+    @api isDebug;
 
     connectedCallback() {
-        console.log('connected TR with fields:',JSON.stringify(this.fields));
-        console.log('connected TR with value:',JSON.stringify(this.value));
+        if (this.isDebug){
+            console.log('connected TR with fields:',JSON.stringify(this.fields));
+            console.log('connected TR with value:',JSON.stringify(this.value));
+        }
     }
 
     handleAction(event) {
-        console.log('handleAction START on ROW with event',event)
-        console.log('handleAction on row',JSON.stringify(this.value));
+        if (this.isDebug) console.log('handleAction START on ROW with event',event)
+        if (this.isDebug) console.log('handleAction on row',JSON.stringify(this.value));
         const actionEvent = new CustomEvent('action', { detail: {
             action : event.detail?.action,
             row: this.value
         }});
-        console.log('handleAction actionEvent prepared ',JSON.stringify(actionEvent));
+        if (this.isDebug) console.log('handleAction actionEvent prepared ',JSON.stringify(actionEvent));
         this.dispatchEvent(actionEvent);
-        console.log('handleAction END on ROW with event',event)
+        if (this.isDebug) console.log('handleAction END on ROW with event',event)
     }
 }
